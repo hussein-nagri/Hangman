fe9bf16e0e4c71760cf34d577aa5b5dc9f8a09eb
hnagri52 <hznagri@uwaterloo.ca>
2020-05-13 14:59:32 -0400

    finished API, starting CLI interface

--- hangman/lib/hangman/game.ex
+++ hangman/lib/hangman/game.ex
@@ -20,41 +20,66 @@ def new_game(word) do
   end
 
   def make_move(game = %{game_state: state}, _guess) when state in [:won, :lost] do
-    {game, tally(game)}
+    game
   end
 
   def make_move(game, guess) do
-    game = accept_move(game, guess, MapSet.member?(game.used, guess) )
-    { game, tally(game) }
+    #validate if it is a lowercase ascii given:  isLower = String.downcase(guess) |> String.equivalent?(guess)
+    accept_move(game, guess, MapSet.member?(game.used, guess) )
   end
 
-  def accept_move(game, guess, _already_guessed = true) do
+  def tally(game) do
+    %{
+    game_state: game.game_state,
+    turns_left: game.turns_left,
+    letters:    game.letters |> reveal_guess(game.used),
+    }
+  end
+
+
+  ###########################################################################################
+
+  defp accept_move(game, guess, _already_guessed = true) do
     Map.put(game, :game_state, :already_used)
   end
 
-  def accept_move(game, guess, _already_gueessed) do
+  defp accept_move(game, guess, _already_gueessed) do
     Map.put(game, :used, MapSet.put(game.used, guess))
     |> score_guess(Enum.member?(game.letters, guess))
   end
 
-  def score_guess(game, _score_guess = true) do
+  defp score_guess(game, _score_guess = true) do
     new_game_state = MapSet.new(game.letters)
     |> MapSet.subset?(game.used)
     |> maybe_won()
     Map.put(game, :game_state, new_game_state)
   end
 
-  def score_guess(game, _not_score_guess) do
-    game
+  defp score_guess(game = %{turns_left: 1} , _not_score_guess) do
+    Map.put(game, :game_state, :lost)
   end
 
 
-  def tally(game) do
-    123
+  defp score_guess(game = %{turns_left: turns_left} , _not_score_guess) do
+      %{
+      game |
+      game_state: :bad_guess,
+      turns_left: turns_left - 1
+      }
   end
 
-  def maybe_won(_does_match = true), do: :won
-  def maybe_won(_doesnt_match), do: :good_guess
+  defp reveal_guess(letters, used) do
+    letters
+    |> Enum.map(fn letter -> reveal_letter(letter, Mapset.member?(used, letter))end)
+  end
+
+  defp reveal_letter(letter, _in_mapset = true), do: letter
+  defp reveal_letter(letter, _not_in_mapset), do: "_"
+
+
+
+  defp maybe_won(_does_match = true), do: :won
+  defp maybe_won(_doesnt_match), do: :good_guess
 
 
 end
