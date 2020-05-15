8e75945e4df90c7f869ded6fceb0d30777d15a83
hnagri52 <hznagri@uwaterloo.ca>
2020-05-15 19:56:52 -0400

    intent to create OTP server

--- dictionary/lib/dictionary/word_list.ex
+++ dictionary/lib/dictionary/word_list.ex
@@ -1,12 +1,13 @@
 defmodule Dictionary.WordList do
 
 
+  @me __MODULE__
   def start_link() do
-    Agent.start_link(fn -> word_list() end)
+    Agent.start_link(fn -> word_list() end, name: @me)
   end
 
-  def random_word(agent) do
-    Agent.get(agent, &Enum.random/1)
+  def random_word() do
+    Agent.get(@me, &Enum.random/1)
   end
 
 
